28ee2052cc9accb52317be1cc5bd86af0b54661f
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2025-06-06 22:05:20 +0200

    update default duration

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,22 @@
-import type { GameMode, GameOption } from "@prisma/client";
+import { GameMode, GameOption } from "@prisma/client";
 import { GAME_OPTIONS } from "./constants";
 
 export const getGameOptionTime = (mode: GameMode, option: GameOption) => {
-  const data = GAME_OPTIONS[mode]?.[option];
-  if (!data) {
-    throw new Error(
-      `Duration not found for mode: ${mode} and option: ${option}`
-    );
-  }
+  try {
+    const data = GAME_OPTIONS[mode]?.[option];
+    if (!data) {
+      throw new Error(
+        `Duration not found for mode: ${mode} and option: ${option}`
+      );
+    }
 
-  return { duration: data.duration, increase: data.increase };
+    return { duration: data.duration, increase: data.increase };
+  } catch (error) {
+    console.error("Error getting game option time", error);
+    const defaultOption = GAME_OPTIONS[GameMode.RAPID][GameOption.RAPID_5];
+    return {
+      duration: defaultOption?.duration ?? 300,
+      increase: defaultOption?.increase ?? 0,
+    };
+  }
 };
