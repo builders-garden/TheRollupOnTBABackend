cac8489858b75e12b865541baf3694396e98a88b
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2025-09-12 01:06:58 +0200

    fix vote also update bullmeter

--- src/handlers/poll-end-handler.ts
+++ src/handlers/poll-end-handler.ts
@@ -1,6 +1,9 @@
 import type { Server } from "socket.io";
-import { ServerToClientSocketEvents } from "../types/enums";
+import { PopupPositions, ServerToClientSocketEvents } from "../types/enums";
 import { LiveTimerManager } from "../lib/timer-manager";
+import { Hex } from "viem";
+import { getBullMeterByPollId } from "../lib/database/queries/bull-meter.query";
+import { env } from "../config/env";
 /**
  * Handle timer expiration for a poll: clear persisted deadline, cleanup timer and notify clients.
  */
@@ -15,13 +18,27 @@ export async function handleTimerExpiration(
     const manager = LiveTimerManager.getInstance();
     manager.deleteTimer(pollId);
 
-    // Notify clients in the room that the poll ended
-    io.to(pollId).emit(ServerToClientSocketEvents.END_SENTIMENT_POLL, {
-      id: pollId,
-      votes: 0,
-      voters: 0,
-      results: { bullPercent: 0, bearPercent: 0 },
-    });
+    const bullmeter = await getBullMeterByPollId(pollId as Hex);
+
+    if (bullmeter) {
+      const totalVotes =
+        (bullmeter.totalYesVotes || 0) + (bullmeter.totalNoVotes || 0);
+      const deadline = new Date((bullmeter.deadline || 0) * 1000);
+
+      io.emit(ServerToClientSocketEvents.END_SENTIMENT_POLL, {
+        id: bullmeter.pollId,
+        pollQuestion: bullmeter.prompt,
+        qrCodeUrl: `https://${env.APP_URL}/poll/${bullmeter.pollId}`,
+        endTime: deadline,
+        position: PopupPositions.TOP_CENTER,
+        votes: bullmeter.totalYesVotes || 0,
+        voters: bullmeter.totalNoVotes || 0,
+        results: {
+          bullPercent: (bullmeter.totalYesVotes || 0) / totalVotes,
+          bearPercent: (bullmeter.totalNoVotes || 0) / totalVotes,
+        },
+      });
+    }
 
     console.log(`[TIMER EXPIRED] Completed expiration handling for ${pollId}`);
   } catch (error) {
--- src/handlers/vote-casted.ts
+++ src/handlers/vote-casted.ts
@@ -1,5 +1,7 @@
+import { Hex } from "viem";
+import { getBullMeterByPollId } from "../lib/database/queries/bull-meter.query";
 import { VoteCastedEvent } from "../types";
-import { ServerToClientSocketEvents } from "../types/enums";
+import { PopupPositions, ServerToClientSocketEvents } from "../types/enums";
 import { SocketHandler } from "./socket-handler";
 
 export class VoteCastedHandler extends SocketHandler {
@@ -12,8 +14,6 @@ export class VoteCastedHandler extends SocketHandler {
     position,
   }: VoteCastedEvent) {
     try {
-      // TODO update db with vote
-
       this.emitToStream(ServerToClientSocketEvents.VOTE_RECEIVED, {
         username,
         profilePicture,
@@ -22,6 +22,26 @@ export class VoteCastedHandler extends SocketHandler {
         promptId,
         position,
       });
+
+      // get bullmeter from pollId
+      const bullmeter = await getBullMeterByPollId(promptId as Hex);
+      if (bullmeter) {
+        const totalVotes =
+          (bullmeter.totalYesVotes || 0) + (bullmeter.totalNoVotes || 0);
+        const deadline = new Date((bullmeter.deadline || 0) * 1000);
+
+        this.emitToStream(ServerToClientSocketEvents.UPDATE_SENTIMENT_POLL, {
+          id: bullmeter.pollId,
+          endTime: deadline,
+          position: PopupPositions.TOP_CENTER,
+          votes: totalVotes || 0,
+          voters: totalVotes || 0,
+          results: {
+            bullPercent: (bullmeter.totalYesVotes || 0) / totalVotes,
+            bearPercent: (bullmeter.totalNoVotes || 0) / totalVotes,
+          },
+        });
+      }
     } catch (e) {
       this.emitToStream(ServerToClientSocketEvents.ERROR, {
         code: 500,
